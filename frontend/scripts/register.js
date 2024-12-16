669d2a2a864ed6e0936c6b3c22059577ed1ece61
Jackson Costa <jacksoncostaleites2017@gmail.com>
2024-12-16 02:35:48 -0300

    .

--- frontend/scripts/register.js
+++ frontend/scripts/register.js
@@ -7,7 +7,7 @@ async function registerUser(user, password) {
 
     const userData = { user, password };
 
-    const response = await fetch("http://localhost:3000/users", {
+    const response = await fetch("https://anotamentos-pro-2.vercel.app/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
